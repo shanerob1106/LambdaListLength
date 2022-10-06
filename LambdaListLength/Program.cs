2923369d35f4b14445f9d27cb6057fc531d2a2d7
b1106857 <b1106857@TU39053>
2022-10-06 11:19:00 +0100

    Add project files.

---
+++ LambdaListLength/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+List<int> num = new List<int> { 1, 3, 4, 5, 7, 9};
+int length = 4;
+
+Func<IList<int>, int, bool> isSizeTooBig = (list, length) => num.Count > length;
+
+bool isLong = isSizeTooBig(num, length);
+
+Console.WriteLine("Too long?: " + isLong);
